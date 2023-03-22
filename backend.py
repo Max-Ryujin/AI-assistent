bfd00b2a649844bb20a7b5f65471f264f14ef1e7
Max-Ryujin <max.kannen@gmail.com>
2023-03-22 21:45:21 +0100

    updated install.bat

--- backend.py
+++ backend.py
@@ -1,4 +1,3 @@
-import tiktoken
 import openai
 import pandas as pd
 import numpy as np
--- install.bat
+++ install.bat
@@ -7,28 +7,17 @@ if %ERRORLEVEL% neq 0 (
     exit /b 1
 )
 
-REM Install CustomTkinter
-pip install CustomTkinter
+REM Install requirements
+pip install -r requirements.txt
 
-REM Install OpenAI
-pip install openai
 
-REM Set up environment variable for OpenAI API key
-if "%OSTYPE%"=="linux-gnu" (
-  echo Please enter your OpenAI API key:
-  set /p API_KEY=
-  echo export OPENAI_API_KEY=%API_KEY% >> ~/.bashrc
-  source ~/.bashrc
-) elif "%OSTYPE%"=="win32" (
-  echo Please enter your OpenAI API key:
-  set /p API_KEY=
-  setx OPENAI_API_KEY %API_KEY%
-  setx PATH "%PATH%;C:\Python310\Scripts"
-  echo Please restart your terminal or editor for environment variable changes to take effect.
-) else (
-  echo Unsupported OS
-  exit /b 1
-)
+
+echo Please enter your OpenAI API key:
+set /p API_KEY=
+setx OPENAI_API_KEY %API_KEY%
+setx PATH "%PATH%;C:\Python310\Scripts"
+echo Please restart your terminal or editor for environment variable changes to take effect.
+
 
 echo Installation complete!
 python Assistant.py
